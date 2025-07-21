f5c69f3bca023e809473640701fd7d211ff9728a
medarawebsite <220260316+medarawebsite@users.noreply.github.com>
2025-07-21 21:12:55 +0000

    Published from Shuffle Editor

---
+++ src/components/__structures/AboutUsSection__structures26.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+export default function AboutUsSection__structures26() {
+    return (
+        <React.Fragment>
+            <>
+                <div><div className="py-[10px]">
+    <hr className="border-t border-gray-300 my-4" />
+  </div>
+  <section className="py-16 bg-green-500 bg-contain bg-no-repeat bg-center" style={{background: '50% 50% / contain no-repeat scroll padding-box border-box #004aac'}}>
+    <div className="container px-4 mx-auto">
+      <div className="max-w-3xl mx-auto text-center">
+        <h1 className="font-heading text-3xl sm:text-4xl text-white mt-2 mb-6">Vision Statement</h1>
+        <p className="max-w-2xl mx-auto text-lg leading-8 text-white mb-12">Empowering clinicians and patients with personalized breast cancer risk insights utilizing breakthrough AI imaging technology.</p>
+        <div className="sm:flex items-center justify-center" />
+      </div>
+    </div>
+  </section>
+  <div><div className="py-[10px]">
+      <hr className="border-t border-gray-300 my-4" />
+    </div>
+    <section className="relative pt-16 lg:pb-16 bg-gray-50 overflow-hidden">
+      <img className="hidden lg:block absolute top-0 right-0 h-full w-1/2 object-cover" src="https://static.shuffle.dev/uploads/files/89/891aed3d3e1847e9f17b379d6d326119edf7f96f/kendra.jpg" alt />
+      <div className="container px-4 mx-auto">
+        <div className="w-full lg:w-1/2">
+          <div className="max-w-md xl:max-w-lg">
+            <h2 className="font-heading text-3xl sm:text-4xl mb-6">Our History</h2>
+            <ol className="relative border-s border-gray-200 dark:border-gray-700">
+              <li className="mb-10 ms-4">
+                <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700" />
+                <time className="mb-1 text-sm font-normal leading-none text-gray-900">January 2025</time>
+                <h3 className="text-lg font-semibold dark:text-white text-green-500">medara™ Founded</h3>
+                <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">medara™ was founded with seed funding from the Jacobs Technion-Cornell Institute, laying the foundation for its innovative work in AI-powered medical imaging..</p>
+              </li>
+              <li className="mb-10 ms-4">
+                <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700" />
+                <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">February – June 2025</time>
+                <h3 className="text-lg font-semibold dark:text-white text-green-500">medara™ is growing—and fast.</h3>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400">An advisory board was established and expert consultants were engaged to support ongoing research and accelerate commercialization efforts.</p>
+              </li>
+              <li className="ms-4">
+                <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700" />
+                <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">October 2025</time>
+                <h3 className="text-lg font-semibold dark:text-white text-green-500">medara™  Attends HLTH USA</h3>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400">With support from VentureWell, Medara will be attending HLTH USA 2025 to showcase its AI-powered risk prediction technology and connect with leaders in digital health and innovation.</p>
+              </li>
+            </ol>
+            <div className="flex items-center mb-4" />
+            <div className="flex items-center mb-4" />
+          </div>
+        </div>
+      </div>
+      <img className="block w-full lg:hidden mt-16 object-cover" src="acros-assets/images/features/woman-man-smilling.jpg" alt />
+    </section>
+  </div>
+  <div className="py-[10px]">
+    <hr className="border-t border-gray-300 my-4" />
+  </div>
+</div>
+
+
+            </>
+        </React.Fragment>
+    );
+}
+

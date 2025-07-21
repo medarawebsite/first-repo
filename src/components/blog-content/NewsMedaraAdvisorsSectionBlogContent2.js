c4bca911107c03c06dbb4a5b7b8c20716436cf5a
medarawebsite <220260316+medarawebsite@users.noreply.github.com>
2025-07-21 17:20:50 +0000

    Published from Shuffle Editor

---
+++ src/components/blog-content/NewsMedaraAdvisorsSectionBlogContent2.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export default function NewsMedaraAdvisorsSectionBlogContent2() {
+    return (
+        <React.Fragment>
+            <>
+                <section>
+  <div className="container px-4 mx-auto mb-14">
+    <div className="flex items-center pt-4 mb-14">
+      <a className="inline-block text-xs font-medium uppercase hover:underline" href="#">Home</a>
+      <span className="mx-2 text-gray-300">
+        <svg id="svg_e525ada6a7cc86a8ed1c907556aaba91" width={5} height={8} viewBox="0 0 5 8" fill="none" xmlns="http://www.w3.org/2000/svg" />
+      </span>
+      <a className="inline-block text-xs font-medium uppercase hover:underline" href="#">Blog</a>
+      <span className="mx-2 text-gray-300">
+        <svg id="svg_e525ada6a7cc86a8ed1c907556aaba91" width={5} height={8} viewBox="0 0 5 8" fill="none" xmlns="http://www.w3.org/2000/svg" />
+      </span>
+      <a className="inline-block text-xs text-gray-300 font-medium uppercase" href="#">Article</a>
+    </div>
+    <div className="max-w-3xl">
+      <h3 className="font-heading text-3xl sm:text-4xl mb-8">medara™ Welcomes Leading Advisors to Accelerate Innovation and Impact</h3>
+      <div className="flex items-center">
+        <div className="w-px h-8 mx-4 sm:mx-8 bg-gray-200" />
+        <div className="flex items-center">
+          <svg id="svg_df48b01d71add93206777566d1f4e7bc" width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" />
+          <a className="ml-2 text-sm leading-6 font-medium hover:underline" href="#">June 17, 2025</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="container px-4 mx-auto">
+    <div className="max-w-3xl mx-auto pt-12">
+      <p className="text-lg leading-8">At medara™, we believe that meaningful innovation happens when deep expertise meets a shared vision to improve patient care. We're thrilled to announce the addition of three exceptional advisors whose experience spans healthcare operations, clinical care, AI, and enterprise strategy—each bringing invaluable perspective to support our mission of transforming cancer risk assessment through imaging and technology. As we continue to build and validate our AI-powered risk assessment tools, we are honored to have these visionary leaders guiding our work. Together, we’re driving forward the future of personalized, imaging-based cancer care—starting with breast cancer.</p>
+    </div>
+  </div>
+</section>
+
+
+            </>
+        </React.Fragment>
+    );
+}
+

9482dfefe510d4fca7383b778032eaa0c04649c3
medarawebsite <220260316+medarawebsite@users.noreply.github.com>
2025-07-21 17:14:52 +0000

    Published from Shuffle Editor

---
+++ src/components/__structures/IndexSection__structures50.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export default function IndexSection__structures50() {
+    return (
+        <React.Fragment>
+            <>
+                <div><h1 className="font-heading text-4xl sm:text-5xl mb-4 text-center pt-10 pb-[30px] text-[#004aac]">Our Partners</h1>
+  <section className="py-16">
+    <div className="container px-4 mx-auto">
+      <h3 className="font-heading text-lg font-medium mb-6">Trusted by brands all over the world</h3>
+      <div className="flex flex-wrap -mx-4 -mb-8">
+        <div className="w-full sm:w-1/2 lg:w-1/4 px-4 mb-8">
+          <div className="flex h-32 px-4 items-center justify-center border-r border-transparent sm:border-gray-100">
+            <img className="block" src="https://static.shuffle.dev/uploads/files/89/891aed3d3e1847e9f17b379d6d326119edf7f96f/WCM-MB-LOGO-HZSS2L-CLR-RGB.png" alt />
+          </div>
+        </div>
+        <div className="w-full sm:w-1/2 lg:w-1/4 px-4 mb-8">
+          <div className="flex h-32 px-4 items-center justify-center border-r border-transparent lg:border-gray-100">
+            <img className="block" src="https://static.shuffle.dev/uploads/files/89/891aed3d3e1847e9f17b379d6d326119edf7f96f/logo.png" alt />
+          </div>
+        </div>
+        <div className="w-full sm:w-1/2 lg:w-1/4 px-4 mb-8">
+          <div className="flex h-32 px-4 items-center justify-center border-r border-transparent sm:border-gray-100">
+            <img className="block" src="https://static.shuffle.dev/uploads/files/89/891aed3d3e1847e9f17b379d6d326119edf7f96f/Cornell-Logo-600x152.png" alt />
+          </div>
+        </div>
+        <div className="w-full sm:w-1/2 lg:w-1/4 px-4 mb-8">
+          <div className="flex h-32 px-4 items-center justify-center">
+            <img className="block" src="https://static.shuffle.dev/uploads/files/89/891aed3d3e1847e9f17b379d6d326119edf7f96f/Horizontal-Jacobs-Cornell-Black.png" alt />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+
+
+            </>
+        </React.Fragment>
+    );
+}
+
---
+++ src/components/__structures/IndexSection__structures54.js
@@ -0,0 +1,109 @@
+import React from 'react';
+
+export default function IndexSection__structures54() {
+    return (
+        <React.Fragment>
+            <>
+                <div>
+  <section className="py-16 bg-[#004aac]">
+    <div className="container px-4 mx-auto">
+      <div className="flex flex-wrap items-center -mx-4">
+        <div className="w-full lg:w-1/2 px-4 mb-16 lg:mb-0">
+          <div className="max-w-md xl:max-w-lg mx-auto xl:mx-0">
+            <div className="text-sm mb-2 font-semibold text-white uppercase">
+              <span>HOW IT Works</span>
+            </div>
+            <h1 className="font-heading text-3xl sm:text-4xl mb-6 text-white">medara™ AI Software Analyzes Longitudinal Imaging and Clinical Data</h1>
+            <p className="max-w-sm text-lg leading-8 mb-8 text-white">seamlessly integrating into existing workflows empowering radiologists to make faster decisions and prioritize urgent patients.</p>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2 px-4">
+          <div className="max-w-lg mx-auto py-10 pl-4 pr-6 sm:px-12 xl:px-20 bg-gray-50 rounded-md bg-white">
+            <div>
+              <div className="flex mb-10">
+                <div className="relative flex-shrink-0 flex items-center justify-center w-16 h-16 mr-10 bg-white rounded bg-gray-100">
+                  <div className="absolute bottom-0 right-0 -mb-3 -mr-3 flex items-center justify-center w-6 h-6 text-xs font-semibold text-gray-900 bg-gray-200 rounded-full">1</div>
+                  <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" viewBox="0 0 194.78 194.78" className>  <defs>    <style dangerouslySetInnerHTML={{__html: "      .cls-1 {        fill: #fff;      }      .cls-2 {        fill: #1ca7d3;      }      .cls-3 {        fill: #1da7d3;      }      .cls-4 {        fill: #154ea2;      }    " }} /></defs>  <path className="cls-4" d="M90.9,9.73C46.08,9.73,9.73,46.08,9.73,90.9h0c0,44.82,36.34,81.16,81.16,81.16s81.16-36.34,81.16-81.16h0c0-44.82-36.32-81.16-81.16-81.16Z" />  <path className="cls-1" d="M41.74,100.64h98.32c1.54,0,2.8,1.25,2.8,2.8v61.33c0,.44-.36.8-.8.8H39.74c-.44,0-.8-.36-.8-.8v-61.33c0-1.55,1.25-2.8,2.8-2.8Z" />  <path className="cls-1" d="M116.88,61.68c0,14.34-11.64,25.96-25.99,25.96s-25.97-11.62-25.97-25.96,11.62-25.98,25.97-25.98,25.99,11.63,25.99,25.98Z" />  <path className="cls-2" d="M90.9,0C40.69,0,0,40.69,0,90.9h0c0,50.2,40.69,90.91,90.9,90.91h0c50.21,0,90.9-40.71,90.9-90.91h0C181.8,40.69,141.11,0,90.9,0ZM90.9,162.33c-39.38,0-71.41-32.04-71.41-71.43S51.51,19.48,90.9,19.48s71.44,32.03,71.44,71.42-32.04,71.43-71.44,71.43Z" />  <path className="cls-3" d="M110.39,152.57c0-23.3,18.9-42.2,42.21-42.2s42.19,18.9,42.19,42.2-18.88,42.21-42.19,42.21-42.21-18.89-42.21-42.21Z" />  <path className="cls-1" d="M172.06,159.08h-38.94c-3.6,0-6.5-2.9-6.5-6.5h0c0-3.58,2.9-6.48,6.5-6.48h38.94c3.58,0,6.48,2.9,6.48,6.48h0c0,3.6-2.9,6.5-6.48,6.5Z" />  <path className="cls-1" d="M159.09,133.1v38.96c0,3.58-2.9,6.49-6.5,6.49h0c-3.58,0-6.49-2.91-6.49-6.49v-38.96c0-3.58,2.9-6.49,6.49-6.49h0c3.6,0,6.5,2.91,6.5,6.49Z" /></svg>
+                  <div className="hidden sm:block absolute top-full left-1/2 mt-4 transform -translate-x-1/2 h-18 w-px bg-gray-100" />
+                </div>
+                <div>
+                  <h3 className="font-heading text-lg font-medium mb-4">Patient Imaging Aquired</h3>
+                  <p className="max-w-xxs leading-7">Following acquisition, image sets are sent to PACS.</p>
+                </div>
+              </div>
+              <div className="flex mb-10">
+                <div className="relative flex flex-shrink-0 items-center justify-center w-16 h-16 mr-10 bg-white rounded">
+                  <div className="absolute bottom-0 right-0 -mb-3 -mr-3 flex items-center justify-center w-6 h-6 text-xs font-semibold text-gray-900 bg-gray-200 rounded-full">2</div>
+                  <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 180.28 196.66" className>
+                    <defs>
+                      <style dangerouslySetInnerHTML={{__html: "\n            .cls-1 {\n              fill: #fff;\n            }\n      \n            .cls-2 {\n              fill: #1f3c72;\n            }\n      \n            .cls-3 {\n              fill: #1da7d3;\n            }\n          " }} />
+                    </defs>
+                    <path className="cls-2" d="M161.21,25.8c-11.06-12.53-25.77-20.9-42.18-24.19C72.27-7.77,30.93,25.08,26.21,68.91c-.62,5.87-2.23,11.62-5.06,16.81L0,124.47l18.78,7.5c1.62.66,2.69,2.23,2.69,3.99v23.24c0,4.53,3.67,8.2,8.2,8.2h22.25c4.53,0,8.2,3.67,8.2,8.19v12.88c0,4.53,3.67,8.2,8.2,8.2h82.31c4.53,0,8.2-3.67,8.2-8.2v-15.55c0-20.15,3.95-40.31,13.08-58.28,5.7-11.22,8.42-24.14,8.39-37.37-.03-17.7-5.39-35.95-19.07-51.47Z" />
+                    <path className="cls-3" d="M152.41,75.39c0,27.16-21.99,49.17-49.16,49.17s-49.16-22.01-49.16-49.17,22.03-49.17,49.16-49.17,49.16,22.02,49.16,49.17Z" />
+                    <path className="cls-3" d="M122.92,147.5h-39.32c-1.82,0-3.3-1.47-3.3-3.28h0c0-1.81,1.47-3.27,3.3-3.27h39.32c1.79,0,3.27,1.47,3.27,3.27h0c0,1.81-1.47,3.28-3.27,3.28Z" />
+                    <path className="cls-3" d="M122.92,160.61h-39.32c-1.82,0-3.3-1.46-3.3-3.28h0c0-1.81,1.47-3.27,3.3-3.27h39.32c1.79,0,3.27,1.47,3.27,3.27h0c0,1.82-1.47,3.28-3.27,3.28Z" />
+                    <path className="cls-3" d="M80.71,75.39h45.08c.22,0,.4.18.4.4v58.4c0,.11-.09.2-.2.2h-45.48c-.11,0-.2-.09-.2-.2v-58.4c0-.22.18-.4.4-.4Z" />
+                    <path className="cls-1" d="M106.53,134.39h-6.53v-55.72c0-1.82,1.44-3.28,3.25-3.28h0c1.82,0,3.28,1.47,3.28,3.28v55.72Z" />
+                    <path className="cls-1" d="M116.37,75.39c0,7.24-5.87,13.12-13.12,13.12s-13.11-5.87-13.11-13.12,5.89-13.11,13.11-13.11,13.12,5.87,13.12,13.11Z" />
+                  </svg>
+                  <div className="hidden sm:block absolute top-full left-1/2 mt-4 transform -translate-x-1/2 h-18 w-px bg-gray-100" />
+                </div>
+                <div>
+                  <h3 className="font-heading text-lg font-medium mb-4">medara™ Analysis Performed</h3>
+                  <p className="max-w-xxs leading-7">Upon transmission to PACS, medara performs a comprehensive analysis of all of the patient's imaging and available clinical data to generate a personalized risk assessment and is saved in PACS.</p>
+                </div>
+              </div>
+              <div className="flex">
+                <div className="relative flex flex-shrink-0 items-center justify-center w-16 h-16 mr-10 bg-white rounded">
+                  <div className="absolute bottom-0 right-0 -mb-3 -mr-3 flex items-center justify-center w-6 h-6 text-xs font-semibold text-gray-900 bg-gray-200 rounded-full">3</div>
+                  <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 193.01 193" className>
+                    <defs>
+                      <style dangerouslySetInnerHTML={{__html: "\n            .cls-1 {\n              fill: #fff;\n            }\n      \n            .cls-2 {\n              fill: #1da7d3;\n            }\n      \n            .cls-3 {\n              fill: #154ea2;\n            }\n          " }} />
+                    </defs>
+                    <path className="cls-3" d="M151.19,57.89H9.65c-5.31,0-9.65-4.32-9.65-9.65V9.65C0,4.32,4.34,0,9.65,0h141.55c5.34,0,9.66,4.32,9.66,9.65v38.6c0,5.33-4.32,9.65-9.66,9.65Z" />
+                    <path className="cls-1" d="M143.15,41.81h0c-1.77,0-3.22-1.44-3.22-3.22v-19.3c0-1.78,1.45-3.22,3.22-3.22h0c1.78,0,3.22,1.44,3.22,3.22v19.3c0,1.78-1.44,3.22-3.22,3.22Z" />
+                    <path className="cls-1" d="M123.84,41.81h0c-1.77,0-3.2-1.44-3.2-3.22v-19.3c0-1.78,1.43-3.22,3.2-3.22h0c1.79,0,3.22,1.44,3.22,3.22v19.3c0,1.78-1.43,3.22-3.22,3.22Z" />
+                    <path className="cls-1" d="M104.55,41.81h0c-1.79,0-3.22-1.44-3.22-3.22v-19.3c0-1.78,1.43-3.22,3.22-3.22h0c1.77,0,3.2,1.44,3.2,3.22v19.3c0,1.78-1.43,3.22-3.2,3.22Z" />
+                    <path className="cls-1" d="M85.26,41.81h0c-1.78,0-3.22-1.44-3.22-3.22v-19.3c0-1.78,1.44-3.22,3.22-3.22h0c1.77,0,3.22,1.44,3.22,3.22v19.3c0,1.78-1.45,3.22-3.22,3.22Z" />
+                    <rect className="cls-2" x="12.88" y="57.89" width="135.09" height="9.65" />
+                    <rect className="cls-2" x="12.88" y="125.45" width="135.09" height="9.65" />
+                    <path className="cls-1" d="M38.6,28.95c0,5.32-4.32,9.65-9.65,9.65s-9.66-4.32-9.66-9.65,4.32-9.65,9.66-9.65,9.65,4.32,9.65,9.65Z" />
+                    <path className="cls-2" d="M59.51,28.95c0,2.66-2.15,4.82-4.82,4.82s-4.82-2.16-4.82-4.82,2.17-4.83,4.82-4.83,4.82,2.16,4.82,4.83Z" />
+                    <path className="cls-3" d="M151.19,125.45H9.65c-5.31,0-9.65-4.33-9.65-9.65v-38.6c0-5.33,4.34-9.65,9.65-9.65h141.55c5.34,0,9.66,4.32,9.66,9.65v38.6c0,5.33-4.32,9.65-9.66,9.65Z" />
+                    <path className="cls-1" d="M38.6,96.5c0,5.33-4.32,9.65-9.65,9.65s-9.66-4.32-9.66-9.65,4.32-9.65,9.66-9.65,9.65,4.32,9.65,9.65Z" />
+                    <path className="cls-1" d="M143.15,109.37h0c-1.77,0-3.22-1.44-3.22-3.22v-19.29c0-1.78,1.45-3.22,3.22-3.22h0c1.78,0,3.22,1.44,3.22,3.22v19.29c0,1.78-1.44,3.22-3.22,3.22Z" />
+                    <path className="cls-1" d="M123.84,109.37h0c-1.77,0-3.2-1.44-3.2-3.22v-19.29c0-1.78,1.43-3.22,3.2-3.22h0c1.79,0,3.22,1.44,3.22,3.22v19.29c0,1.78-1.43,3.22-3.22,3.22Z" />
+                    <path className="cls-1" d="M104.55,109.37h0c-1.79,0-3.22-1.44-3.22-3.22v-19.29c0-1.78,1.43-3.22,3.22-3.22h0c1.77,0,3.2,1.44,3.2,3.22v19.29c0,1.78-1.43,3.22-3.2,3.22Z" />
+                    <path className="cls-1" d="M85.26,109.37h0c-1.78,0-3.22-1.44-3.22-3.22v-19.29c0-1.78,1.44-3.22,3.22-3.22h0c1.77,0,3.22,1.44,3.22,3.22v19.29c0,1.78-1.45,3.22-3.22,3.22Z" />
+                    <path className="cls-2" d="M59.51,96.5c0,2.67-2.15,4.82-4.82,4.82s-4.82-2.15-4.82-4.82,2.17-4.82,4.82-4.82,4.82,2.16,4.82,4.82Z" />
+                    <path className="cls-3" d="M144.77,193H9.65c-5.31,0-9.65-4.32-9.65-9.65v-38.59c0-5.33,4.34-9.65,9.65-9.65h141.55c5.34,0,9.66,4.32,9.66,9.65v32.17c0,8.88-7.21,16.08-16.09,16.08Z" />
+                    <path className="cls-1" d="M85.26,176.92h0c-1.78,0-3.22-1.44-3.22-3.22v-19.3c0-1.77,1.44-3.21,3.22-3.21h0c1.77,0,3.22,1.44,3.22,3.21v19.3c0,1.78-1.45,3.22-3.22,3.22Z" />
+                    <path className="cls-1" d="M104.55,176.92h0c-1.79,0-3.22-1.44-3.22-3.22v-19.3c0-1.77,1.43-3.21,3.22-3.21h0c1.77,0,3.2,1.44,3.2,3.21v19.3c0,1.78-1.43,3.22-3.2,3.22Z" />
+                    <path className="cls-1" d="M38.6,164.05c0,5.33-4.32,9.65-9.65,9.65s-9.66-4.32-9.66-9.65,4.32-9.65,9.66-9.65,9.65,4.33,9.65,9.65Z" />
+                    <path className="cls-2" d="M59.51,164.05c0,2.66-2.15,4.82-4.82,4.82s-4.82-2.16-4.82-4.82,2.17-4.82,4.82-4.82,4.82,2.16,4.82,4.82Z" />
+                    <path className="cls-3" d="M144.77,99.72c-24.89,0-45.04,20.16-45.04,45.03s20.16,45.03,45.04,45.03,45.02-20.16,45.02-45.03h0c0-24.88-20.17-45.03-45.02-45.03Z" />
+                    <path className="cls-1" d="M151.54,155.73h-13.56c-11.22,0-20.31,9.09-20.31,20.31l27.1,10.16,27.08-10.16c0-11.22-9.1-20.31-20.31-20.31Z" />
+                    <path className="cls-1" d="M158.31,135.41c0,7.48-6.06,13.54-13.54,13.54s-13.56-6.06-13.56-13.54,6.06-13.54,13.56-13.54,13.54,6.06,13.54,13.54Z" />
+                    <path className="cls-2" d="M144.77,96.5c-26.66,0-48.25,21.6-48.25,48.25s21.59,48.25,48.25,48.25,48.25-21.6,48.25-48.25h0c0-26.65-21.6-48.25-48.25-48.25ZM144.77,183.34c-21.3,0-38.62-17.31-38.62-38.59s17.31-38.61,38.62-38.61,38.58,17.32,38.58,38.61-17.31,38.59-38.58,38.59Z" />
+                  </svg>
+                  <div className="hidden sm:block absolute top-full left-1/2 mt-4 transform -translate-x-1/2 h-18 w-px bg-gray-100" />
+                </div>
+                <div>
+                  <h3 className="font-heading text-lg font-medium mb-4">Prioritization &amp; Image Review</h3>
+                  <p className="max-w-xxs leading-7">medara™ prioritizes cases based on risk within the radiologist's case list, displaying the patient-specific risk score directly within the imaging set upon radiologist access.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+
+
+            </>
+        </React.Fragment>
+    );
+}
+

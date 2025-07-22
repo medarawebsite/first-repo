d0a50fa0064a4e68985cdbc585edae6dcafbdb6f
medarawebsite <220260316+medarawebsite@users.noreply.github.com>
2025-07-22 20:25:24 +0000

    Published from Shuffle Editor

---
+++ src/components/logo-clouds/IndexSectionLogoClouds49.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function IndexSectionLogoClouds49() {
+    return (
+        <React.Fragment>
+            <>
+                <style dangerouslySetInnerHTML={{__html: "\n        @keyframes scroll {\n          0% { transform: translateX(0%); }\n          100% { transform: translateX(-100%); }\n        }\n      \n        .animate-scroll {\n          animation: scroll 20s linear infinite;\n        }\n      " }} />
+
+
+            </>
+        </React.Fragment>
+    );
+}
+

aa2be4b38a6c0653e182c27a83f8a8925b0604b5
medarawebsite <220260316+medarawebsite@users.noreply.github.com>
2025-07-23 18:17:30 +0000

    Published from Shuffle Editor

---
+++ src/components/__structures/AboutUsSection__structures6.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+export default function AboutUsSection__structures6() {
+    return (
+        <React.Fragment>
+            <>
+                <div className="flex flex-wrap -mx-4 -mb-4 md:mb-0">
+  <div className="w-full md:w-1/2 px-4 mb-4 md:mb-0">
+    <section>
+      <div className="relative h-140 w-full">
+        <a className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 group flex items-center justify-center w-24 h-24 rounded-full overflow-hidden" href="#">
+          <span className="relative">
+          </span>
+        </a>
+      </div>
+      <div className="hidden fixed top-0 left-0 bottom-0 w-5/6 max-w-sm z-50">
+        <div className="fixed inset-0 backdrop-blur-xl backdrop-filter bg-gray-900 bg-opacity-80" />
+        <nav className="relative pt-7 pb-8 bg-white h-full overflow-y-auto">
+          <div className="flex flex-col px-6 h-full">
+            <a className="inline-block ml-4 mb-7" href="#">
+              <img src="acros-assets/logo/logo-acros-black.svg" alt />
+            </a>
+            <ul className="w-full mb-auto pb-16">
+              <li><a className="font-heading block text-base font-medium py-4 px-6 hover:bg-green-50 rounded-sm" href="#">Home</a></li>
+              <li><a className="font-heading block text-base py-4 px-6 hover:bg-green-50 rounded-sm" href="#">About</a></li>
+              <li><a className="font-heading block text-base py-4 px-6 hover:bg-green-50 rounded-sm" href="#">Company</a></li>
+              <li><a className="font-heading block text-base py-4 px-6 hover:bg-green-50 rounded-sm" href="#">Services</a></li>
+              <li><a className="font-heading block text-base py-4 px-6 hover:bg-green-50 rounded-sm" href="#">Testimonials</a></li>
+              <li><a className="font-heading block text-base py-4 px-6 hover:bg-green-50 rounded-sm" href="#">Contact</a></li>
+            </ul>
+            <div className="w-full">
+              <a className="block w-full py-4 px-4 mb-4 text-center font-heading font-medium text-base hover:text-green-500 border border-gray-900 hover:border-green-500 rounded-sm transition duration-200" href="#">Log in</a><a className="block w-full py-4 px-4 mb-8 text-center font-heading font-medium text-base text-white bg-green-500 hover:bg-green-600 border border-green-500 hover:border-green-600 rounded-sm transition duration-200" href="#">Sign up</a>
+              <p className="pl-2 text-sm">2022 © Shuffle</p>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </section>
+  </div>
+</div>
+
+
+            </>
+        </React.Fragment>
+    );
+}
+

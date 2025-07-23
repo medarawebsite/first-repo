3acb308ec112cdd7ad7fa08a07fa77c37f68e77e
medarawebsite <220260316+medarawebsite@users.noreply.github.com>
2025-07-23 18:12:11 +0000

    Published from Shuffle Editor

---
+++ src/components/__structures/NewsSection__structures3.js
@@ -0,0 +1,199 @@
+import React from 'react';
+
+export default function NewsSection__structures3() {
+    return (
+        <React.Fragment>
+            <>
+                <div className="container mx-auto">
+  <section className="py-16 bg-white">
+    <div className="container px-4 mx-auto">
+      <div className="flex flex-wrap items-center -mx-4 mb-12">
+        <div className="w-full xl:w-2/3 px-4 mb-8 xl:mb-0">
+          <div className="text-sm mb-2 font-semibold text-gray-900 uppercase">
+            <span>Expand your</span>
+            <span className="text-green-500">knowledge</span>
+          </div>
+          <h2 className="font-heading text-3xl sm:text-4xl max-w-max">Check out the Latest Updates</h2>
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-4 -mb-12">
+        <div className="w-full lg:w-1/2 px-4 mb-12">
+          <div className="sm:flex">
+            <img className="w-40 h-40 rounded mb-6 sm:mb-0 sm:mr-8" src="acros-assets/images/blog/work-man-160x160.jpg" alt />
+            <div>
+              <span className="text-sm font-semibold uppercase text-gray-200">10 JUN 2025</span>
+              <h3 className="font-heading font-medium text-lg mt-2 mb-4">There are many variations of passages of Lorem Ipsum available</h3>
+              <p className="leading-7 mb-4">There are many variations of passages of Lorem Ipsum available.</p>
+              <a className="group inline-flex items-center font-heading font-medium" href="#">
+                <span className="mr-4">Read more</span>
+                <div className="flex items-center justify-center w-10 h-10 rounded-full bg-green-50 group-hover:bg-green-100">
+                  <svg id="svg_af7d8f84c1be2e2663f0a4b184d06fd1" width={10} height={8} viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg" />
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2 px-4 mb-12">
+          <div className="sm:flex">
+            <img className="w-40 h-40 rounded mb-6 sm:mb-0 sm:mr-8" src="acros-assets/images/blog/woman-160x160.jpg" alt />
+            <div>
+              <span className="text-sm font-semibold uppercase text-gray-200">10 JUN 2025</span>
+              <h3 className="font-heading font-medium text-lg mt-2 mb-4">There are many variations of passages of Lorem Ipsum available</h3>
+              <p className="leading-7 mb-4">There are many variations of passages of Lorem Ipsum available.</p>
+              <a className="group inline-flex items-center font-heading font-medium" href="#">
+                <span className="mr-4">Read more</span>
+                <div className="flex items-center justify-center w-10 h-10 rounded-full bg-green-50 group-hover:bg-green-100">
+                  <svg id="svg_af7d8f84c1be2e2663f0a4b184d06fd1" width={10} height={8} viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg" />
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2 px-4 mb-12">
+          <div className="sm:flex">
+            <img className="w-40 h-40 rounded mb-6 sm:mb-0 sm:mr-8" src="acros-assets/images/blog/robot-160x160.jpg" alt />
+            <div>
+              <span className="text-sm font-semibold uppercase text-gray-200">10 JUN 2025</span>
+              <h3 className="font-heading font-medium text-lg mt-2 mb-4">There are many variations of passages of Lorem Ipsum available</h3>
+              <p className="leading-7 mb-4">There are many variations of passages of Lorem Ipsum available.</p>
+              <a className="group inline-flex items-center font-heading font-medium" href="#">
+                <span className="mr-4">Read more</span>
+                <div className="flex items-center justify-center w-10 h-10 rounded-full bg-green-50 group-hover:bg-green-100">
+                  <svg id="svg_af7d8f84c1be2e2663f0a4b184d06fd1" width={10} height={8} viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg" />
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2 px-4 mb-12">
+          <div className="sm:flex">
+            <img className="w-40 h-40 rounded mb-6 sm:mb-0 sm:mr-8" src="acros-assets/images/blog/programming-160x160.jpg" alt />
+            <div>
+              <span className="text-sm font-semibold uppercase text-gray-200">10 JUN 2025</span>
+              <h3 className="font-heading font-medium text-lg mt-2 mb-4">There are many variations of passages of Lorem Ipsum available</h3>
+              <p className="leading-7 mb-4">There are many variations of passages of Lorem Ipsum available.</p>
+              <a className="group inline-flex items-center font-heading font-medium" href="#">
+                <span className="mr-4">Read more</span>
+                <div className="flex items-center justify-center w-10 h-10 rounded-full bg-green-50 group-hover:bg-green-100">
+                  <svg id="svg_af7d8f84c1be2e2663f0a4b184d06fd1" width={10} height={8} viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg" />
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2 px-4 mb-12">
+          <div className="sm:flex">
+            <img className="w-40 h-40 rounded mb-6 sm:mb-0 sm:mr-8" src="acros-assets/images/blog/work-man-160x160.jpg" alt />
+            <div>
+              <span className="text-sm font-semibold uppercase text-gray-200">10 JUN 2025</span>
+              <h3 className="font-heading font-medium text-lg mt-2 mb-4">Unveiling the Layers of Lorem Ipsum</h3>
+              <p className="leading-7 mb-4">Unveiling the Layers of Lorem Ipsum.</p>
+              <a className="group inline-flex items-center font-heading font-medium" href="#">
+                <span className="mr-4">Read more</span>
+                <div className="flex items-center justify-center w-10 h-10 rounded-full bg-green-50 group-hover:bg-green-100">
+                  <svg id="svg_af7d8f84c1be2e2663f0a4b184d06fd1" width={10} height={8} viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg" />
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2 px-4 mb-12">
+          <div className="sm:flex">
+            <img className="w-40 h-40 rounded mb-6 sm:mb-0 sm:mr-8" src="acros-assets/images/blog/woman-160x160.jpg" alt />
+            <div>
+              <span className="text-sm font-semibold uppercase text-gray-200">10 JUN 2025</span>
+              <h3 className="font-heading font-medium text-lg mt-2 mb-4">Unveiling the Layers of Lorem Ipsum</h3>
+              <p className="leading-7 mb-4">Unveiling the Layers of Lorem Ipsum.</p>
+              <a className="group inline-flex items-center font-heading font-medium" href="#">
+                <span className="mr-4">Read more</span>
+                <div className="flex items-center justify-center w-10 h-10 rounded-full bg-green-50 group-hover:bg-green-100">
+                  <svg id="svg_af7d8f84c1be2e2663f0a4b184d06fd1" width={10} height={8} viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg" />
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div className="container mx-auto"><section className="py-16 bg-[#004aac]">
+      <div className="container px-4 mx-auto">
+        <div className="flex flex-wrap items-center -mx-4">
+          <div className="w-full lg:w-1/2 px-4 mb-16 lg:mb-0">
+            <div className="max-w-lg mx-auto lg:mx-0 mb-6">
+              <h2 className="font-heading text-3xl sm:text-4xl mt-2 text-white">Stay in the loop! Subscribe for medara™ insights, news, and breakthroughs.</h2>
+            </div>
+          </div>
+          <div className="w-full lg:w-1/2 px-4">
+            <div className="max-w-lg mx-auto lg:mr-0 py-10 px-6 sm:px-12 bg-white rounded-md">
+              <h3 className="font-heading font-medium text-2xl mb-4">Sign up Today!</h3>
+              <form action>
+                <input className="block w-full p-4 mb-4 font-heading text-gray-300 placeholder-gray-300 border border-gray-100 rounded-sm outline-none" type="text" placeholder="Name" fdprocessedid="z5hxgm" />
+                <input className="block w-full p-4 mb-4 font-heading text-gray-300 placeholder-gray-300 border border-gray-100 rounded-sm outline-none" type="email" placeholder="E-mail" fdprocessedid="oly4e" />
+                <button className="inline-block w-full py-4 px-6 text-center font-heading font-medium text-black text-base bg-white hover:bg-gray-100 border border-gray-300 rounded-sm transition duration-200" type="submit" fdprocessedid="udx507">Sign up</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section className="py-24 bg-gray-900">
+      <div className="container px-4 mx-auto">
+        <div className="flex flex-wrap justify-between -mx-4 pb-20">
+          <div className="w-full lg:w-1/4 xl:w-2/6 px-4 mb-16 lg:mb-0">
+            <a className="inline-block mb-4" href="#">
+              <img src="https://static.shuffle.dev/uploads/files/89/891aed3d3e1847e9f17b379d6d326119edf7f96f/White-logo-no-background-png-1752167098784.webp" alt />
+            </a>
+            <span className="block text-gray-300 mb-6">*The device described has not been evaluated by the FDA and is not commercially available in the United States.</span>
+            <span className="block text-gray-300 mb-6">2025 © medara™</span>
+            <a className="inline-block font-heading text-white hover:text-gray-100 mr-8" href="#">Terms</a><a className="inline-block font-heading text-white hover:text-gray-100" href="#">Privacy</a>
+          </div>
+          <div className="w-full sm:w-1/2 lg:w-1/4 xl:w-auto xl:mr-6 px-4 mb-16 lg:mb-0">
+            <h4 className="font-heading text-lg text-white mb-6">Contact</h4>
+            <div className="flex items-center mb-2">
+              <div className="flex items-center justify-center w-10 h-10 mr-3 border border-gray-500 rounded-full">
+                <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 187.11 187.11" className>
+                  <defs />
+                  <path className="cls-1" d="M94.7,180.56c-46.7.94-89.28-39.1-87.79-86.82C5.79,46.04,47.44,4.87,94.89,6.75c50.06.56,84.61,42.66,85.33,82.86,2.83,41.56-30.05,89.83-85.52,90.96h0ZM0,93.75c.42,51.76,42.76,93.94,94.7,93.35,51.18.2,95.26-46.02,92.26-97.45C184.73,40.61,144.2.76,94.89.01,44.08-.74.75,39.68,0,93.75h0Z" />
+                  <path className="cls-2" d="M62.78,46.99c-11.63,0-19.56,18.23-14.38,31.95,6.43,22.25,37.01,57.56,65.13,60.91,8.33,1.42,25.45-2.79,26.8-14.36-.7-9.11-22.36-22.43-28.14-18.96-8.31,5.77-11.69,10.55-24.96-2.77-4.89-4.91-13.27-15.42-12.6-22.31.38-3.91,7.82-2.34,7.05-9.85-.65-6.37-12.27-24.62-18.9-24.62h0Z" />
+                </svg>
+              </div>
+              <span className="font-heading text-white">+1 646-484-8306</span>
+            </div>
+            <div className="flex items-center mb-4">
+              <div className="flex items-center justify-center w-10 h-10 mr-3 border border-gray-500 rounded-full">
+                <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 191.84 191.84" className>
+                  <defs />
+                  <path className="cls-1" d="M97.1,185.13c-47.88.96-91.54-40.09-90.01-89.01C5.94,47.2,48.64,5,97.29,6.92c51.32.58,86.75,43.74,87.49,84.96,2.9,42.61-30.81,92.11-87.68,93.26h0ZM0,96.12c.43,53.07,43.85,96.32,97.1,95.71,52.47.2,97.67-47.18,94.6-99.92C189.4,41.64,147.85.78,97.29.01,45.2-.76.77,40.68,0,96.12h0Z" />
+                  <path className="cls-2" d="M43.53,58.29l52.09,45.17,52.98-45.17c-10.68,0-101.35,0-105.07,0h0ZM153.32,62.42l-38.99,33.49,39.15,33.07c0-3.69-.16-62.44-.16-66.57h0ZM38.36,62.57v66.55l38.99-33.21-38.99-33.35h0ZM109.5,100.07l-11.51,9.5c-1.32,1.13-3.44,1.13-4.76,0l-11.11-9.54-38.9,33.53c35.08,0,70.16,0,105.23,0l-38.95-33.49h0Z" />
+                </svg>
+              </div>
+              <span className="font-heading text-white">info@medara.co</span>
+            </div>
+            <span className="block text-sm text-gray-300">medara, Inc.</span>
+            <span className="block text-sm text-gray-300">11 E Loop Rd Suite 381</span>
+            <span className="block text-sm text-gray-300">New York, NY 10044</span>
+          </div>
+          <div className="w-full sm:w-1/2 lg:w-1/4 xl:w-auto xl:mr-20 px-4 mb-16 lg:mb-0">
+            <h4 className="font-heading text-lg text-white mb-8">Pages</h4>
+            <ul>
+              <li className="mb-6"><a className="font-heading text-white hover:text-gray-100" href="/aboutus">About</a></li>
+              <li className="mb-6"><a className="font-heading text-white hover:text-gray-100" href="/technology">Technology</a></li>
+              <li className="mb-6"><a className="font-heading text-white hover:text-gray-100" href="/news">Media Room</a></li>
+              <li className="mb-6"><a className="font-heading text-white hover:text-gray-100" href="for_patients.html">For Patients</a></li>
+              <li><a className="font-heading text-white hover:text-gray-100" href="contact.html">Contact</a></li>
+            </ul>
+          </div>
+          <div className="w-full lg:w-1/4 xl:w-auto px-4" />
+        </div>
+      </div>
+    </section>
+  </div>
+</div>
+
+
+            </>
+        </React.Fragment>
+    );
+}
+

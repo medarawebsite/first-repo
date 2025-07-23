dce94467e73dde9050415a785c6b938c9a4733a2
medarawebsite <220260316+medarawebsite@users.noreply.github.com>
2025-07-23 18:16:44 +0000

    Published from Shuffle Editor

---
+++ src/components/features/AboutUsSectionFeatures31.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+export default function AboutUsSectionFeatures31() {
+    return (
+        <React.Fragment>
+            <>
+                <section className="relative pt-16 lg:pb-16 bg-gray-50 overflow-hidden">
+  <img className="hidden lg:block absolute top-0 right-0 h-full w-1/2 object-cover" src="https://static.shuffle.dev/uploads/files/89/891aed3d3e1847e9f17b379d6d326119edf7f96f/work.jpg" alt />
+  <div className="container px-4 mx-auto">
+    <div className="w-full lg:w-1/2">
+      <div className="max-w-md xl:max-w-lg">
+        <h2 className="font-heading text-3xl sm:text-4xl mb-6">Our History</h2>
+        <ol className="relative border-s border-gray-200 dark:border-gray-700">
+          <li className="mb-10 ms-4">
+            <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700" />
+            <time className="mb-1 text-sm font-normal leading-none text-gray-900">January 2025</time>
+            <h3 className="text-lg font-semibold dark:text-white text-green-500">medara™ Founded</h3>
+            <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">medara™ was founded with seed funding from the Jacobs Technion-Cornell Institute, laying the foundation for its innovative work in AI-powered medical imaging..</p>
+          </li>
+          <li className="mb-10 ms-4">
+            <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700" />
+            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">February – June 2025</time>
+            <h3 className="text-lg font-semibold dark:text-white text-green-500">medara™ is growing—and fast.</h3>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400">An advisory board was established and expert consultants were engaged to support ongoing research and accelerate commercialization efforts.</p>
+          </li>
+          <li className="ms-4">
+            <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700" />
+            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">October 2025</time>
+            <h3 className="text-lg font-semibold dark:text-white text-green-500">medara™  Attends HLTH USA</h3>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400">With support from VentureWell, Medara will be attending HLTH USA 2025 to showcase its AI-powered risk prediction technology and connect with leaders in digital health and innovation.</p>
+          </li>
+        </ol>
+        <div className="flex items-center mb-4" />
+        <div className="flex items-center mb-4" />
+      </div>
+    </div>
+  </div>
+  <img className="block w-full lg:hidden mt-16 object-cover" src="acros-assets/images/features/woman-man-smilling.jpg" alt />
+</section>
+
+
+            </>
+        </React.Fragment>
+    );
+}
+

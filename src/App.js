b61e8b7bb5362a572ce4e734de614f20e823fbaa
medarawebsite <220260316+medarawebsite@users.noreply.github.com>
2025-07-25 19:29:20 +0000

    Published from Shuffle Editor

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  Routes,
+  Route
+} from 'react-router-dom';
+
+import IndexPage from './pages/Index.js';
+import TechnologyPage from './pages/Technology.js';
+import NewsMedaraAdvisorsPage from './pages/News_medara_advisors.js';
+import ForPatientsPage from './pages/For_patients.js';
+import ContactUsPage from './pages/Contact_us.js';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<IndexPage />} />
+         
+      <Route path="/technology" element={<TechnologyPage />} />
+         
+      <Route path="/newsmedaraadvisors" element={<NewsMedaraAdvisorsPage />} />
+         
+      <Route path="/forpatients" element={<ForPatientsPage />} />
+         
+      <Route path="/contactus" element={<ContactUsPage />} />
+    </Routes>
+  );
+}
+
+export default App;

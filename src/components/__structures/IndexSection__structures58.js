99e49a9a7a4a167d398994e13ee21f696d4cdcf5
medarawebsite <220260316+medarawebsite@users.noreply.github.com>
2025-07-28 16:03:47 +0000

    Published from Shuffle Editor

---
+++ src/components/__structures/IndexSection__structures58.js
@@ -0,0 +1,130 @@
+import React from 'react';
+
+export default function IndexSection__structures58() {
+    return (
+        <React.Fragment>
+            <>
+                <div><div><section>
+      <nav className="py-4 bg-white">
+        <div className="container mx-auto px-4">
+          <div className="relative flex items-center justify-between">
+            <div className="w-auto">
+              <a className="inline-block" href="#">
+                <img src="https://static.shuffle.dev/uploads/files/89/891aed3d3e1847e9f17b379d6d326119edf7f96f/medara-small.png" alt />
+              </a>
+            </div>
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 hidden lg:block">
+              <ul className="flex items-center">
+                <li className="font-heading mr-12 text-base"><a className="hover:text-black" href="about_us.html">About</a></li>
+                <li className="font-heading mr-12 text-base"><a className="hover:text-black" href="/technology">Technology</a></li>
+                <li className="font-heading mr-12 text-base"><a className="hover:text-black" href="news.html">News</a></li>
+                <li className="font-heading mr-12 text-base"><a className="hover:text-black" href="/forpatients">For Patients</a></li>
+              </ul>
+            </div>
+            <div className="w-auto hidden lg:block"><a className="inline-block py-4 px-8 font-heading font-medium text-base text-white bg-green-500 hover:bg-green-600 border border-green-500 hover:border-green-600 rounded-sm transition duration-200" href="/contactus">Contact</a></div>
+            <div className="w-auto lg:hidden">
+              <button className="inline-flex w-14 h-14 justify-center items-center bg-gray-500 hover:bg-gray-600 rounded-full">
+                <svg width={20} height={10} viewBox="0 0 20 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M19 9H1M19 1H1" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div className="pt-24 sm:pt-34 pb-8 bg-cover" style={{backgroundImage: 'url("https://static.shuffle.dev/uploads/files/89/891aed3d3e1847e9f17b379d6d326119edf7f96f/photo-1587664289285-e6fb47cbe370-1752163934279-webp-1752167764826.webp")'}}>
+        <div className="container px-4 mx-auto">
+          <div className="max-w-2xl xl:max-w-4xl mx-auto text-center">
+            <div className="text-sm mb-2 font-semibold text-white uppercase" />
+            <h1 className="font-heading text-4xl md:text-5xl text-white mb-6">A New Era in Risk Prediction</h1>
+            <div className="sm:flex items-center justify-center mb-24"><a className="inline-block w-full sm:w-auto py-4 px-6 mb-4 sm:mb-0 sm:mr-4 text-center font-heading font-medium text-base text-white bg-green-500 hover:bg-green-600 border border-green-500 hover:border-green-600 rounded-sm transition duration-200" href="#">Keep Scrolling</a></div>
+            <a className="inline-block text-white" href="#">
+              <svg width={26} height={30} viewBox="0 0 26 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M13 1V29M13 29L25 17M13 29L1 17" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="hidden fixed top-0 left-0 bottom-0 w-5/6 max-w-sm z-50">
+        <div className="fixed inset-0 backdrop-blur-xl backdrop-filter bg-gray-900 bg-opacity-80" />
+        <nav className="relative pt-7 pb-8 bg-white h-full overflow-y-auto">
+          <div className="flex flex-col px-6 h-full">
+            <a className="inline-block ml-4 mb-7" href="#">
+              <img src="acros-assets/logo/logo-acros-black.svg" alt />
+            </a>
+            <ul className="w-full mb-auto pb-16">
+              <li><a className="font-heading block text-base font-medium py-4 px-6 hover:bg-green-50 rounded-sm" href="#">Home</a></li>
+              <li><a className="font-heading block text-base py-4 px-6 hover:bg-green-50 rounded-sm" href="#">About</a></li>
+              <li><a className="font-heading block text-base py-4 px-6 hover:bg-green-50 rounded-sm" href="#">Company</a></li>
+              <li><a className="font-heading block text-base py-4 px-6 hover:bg-green-50 rounded-sm" href="#">Services</a></li>
+              <li><a className="font-heading block text-base py-4 px-6 hover:bg-green-50 rounded-sm" href="#">Testimonials</a></li>
+              <li><a className="font-heading block text-base py-4 px-6 hover:bg-green-50 rounded-sm" href="#">Contact</a></li>
+            </ul>
+            <div className="w-full">
+              <a className="block w-full py-4 px-4 mb-4 text-center font-heading font-medium text-base hover:text-green-500 border border-gray-900 hover:border-green-500 rounded-sm transition duration-200" href="#">Log in</a><a className="block w-full py-4 px-4 mb-8 text-center font-heading font-medium text-base text-white bg-green-500 hover:bg-green-600 border border-green-500 hover:border-green-600 rounded-sm transition duration-200" href="#">Sign up</a>
+              <p className="pl-2 text-sm">2022 © Shuffle</p>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </section>
+  </div>
+  <div>
+    <section className="py-16 bg-gray-50">
+      <div className="container px-4 mx-auto">
+        <div className="flex flex-wrap items-center -mx-4 mb-16">
+          <div className="w-full xl:w-2/3 px-4 mb-8 xl:mb-0">
+            <h2 className="max-w-3xl font-heading text-3xl sm:text-4xl">Harness the Power of Image-Based AI to Predict Short-Term Cancer Risk</h2>
+          </div>
+          <div className="w-full xl:w-auto ml-auto px-4 xl:text-right"><a className="inline-block w-full sm:w-auto py-4 px-6 text-center font-heading font-medium text-base text-gray-900 hover-custom-blue border border-gray-900 rounded-sm transition duration-150" href="/technology">Get More Details</a></div>
+        </div>
+        <div className="flex flex-wrap -mx-4">
+          <div className="w-full sm:w-1/2 xl:w-1/4 px-4 mb-12 xl:mb-0">
+            <div className="max-w-xs mx-auto">
+              <div className="flex items-center justify-center w-16 h-16 mb-8 bg-white rounded">
+                <img src="https://static.shuffle.dev/uploads/files/89/891aed3d3e1847e9f17b379d6d326119edf7f96f/decision-making.svg" alt="Icon" />
+              </div>
+              <h3 className="font-heading text-lg font-medium mb-4">Enhance Clinical Decision-Making</h3>
+              <p className="leading-7">with the comprehensive analysis of patient imaging and clinical data.</p>
+            </div>
+          </div>
+          <div className="w-full sm:w-1/2 xl:w-1/4 px-4 mb-12 xl:mb-0">
+            <div className="max-w-xs mx-auto">
+              <div className="flex items-center justify-center w-16 h-16 mb-8 bg-white rounded">
+                <img src="https://static.shuffle.dev/uploads/files/89/891aed3d3e1847e9f17b379d6d326119edf7f96f/time.svg" alt="Icon" />
+              </div>
+              <h3 className="font-heading text-lg font-medium mb-4">Enable Timely Intervention</h3>
+              <p className="leading-7">with a personalized five-year patient risk assessment.</p>
+            </div>
+          </div>
+          <div className="w-full sm:w-1/2 xl:w-1/4 px-4 mb-12 sm:mb-0">
+            <div className="max-w-xs mx-auto">
+              <div className="flex items-center justify-center w-16 h-16 mb-8 bg-white rounded">
+                <img src="https://static.shuffle.dev/uploads/files/89/891aed3d3e1847e9f17b379d6d326119edf7f96f/costs-icon-2.svg" alt="Icon" />
+              </div>
+              <h3 className="font-heading text-lg font-medium mb-4">Reduce Healthcare Costs</h3>
+              <p className="leading-7">by detecting cancer early with personalized screening protocols</p>
+            </div>
+          </div>
+          <div className="w-full sm:w-1/2 xl:w-1/4 px-4">
+            <div className="max-w-xs mx-auto">
+              <div className="flex items-center justify-center w-16 h-16 mb-8 bg-white rounded">
+                <img src="https://static.shuffle.dev/uploads/files/89/891aed3d3e1847e9f17b379d6d326119edf7f96f/patient-outcomes.svg" alt="Icon" />
+              </div>
+              <h3 className="font-heading text-lg font-medium mb-4">Improve Patient Outcomes</h3>
+              <p className="leading-7">by prioritizing high-risk patients and detecting cancer early.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</div>
+
+
+            </>
+        </React.Fragment>
+    );
+}
+

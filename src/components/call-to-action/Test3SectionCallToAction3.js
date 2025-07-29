76e802d365befe82f4067211135a779460733037
medarawebsite <220260316+medarawebsite@users.noreply.github.com>
2025-07-29 19:41:06 +0000

    Published from Shuffle Editor

---
+++ src/components/call-to-action/Test3SectionCallToAction3.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function Test3SectionCallToAction3() {
+    return (
+        <React.Fragment>
+            <>
+                <section className="py-20" style={{backgroundImage: 'url("https://static.shuffle.dev/uploads/files/89/891aed3d3e1847e9f17b379d6d326119edf7f96f/background-3.png")', backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat'}}>
+  <div className="container px-4 mx-auto">
+    <div className="flex flex-wrap items-center justify-between -mx-4">
+      <div className="w-full lg:w-2/5 px-4 mb-6 lg:mb-0">
+        <h2 className="max-w-md font-heading text-3xl sm:text-4xl text-white">The latest version of this excellent product</h2>
+      </div>
+      <div className="w-full lg:w-auto px-4 mb-6 lg:mb-0">
+        <p className="max-w-sm text-white leading-7">The standard chunk of Lorem Ipsum used since the 1500s is reproduced.</p>
+      </div>
+      <div className="w-full lg:w-auto px-4"><a className="inline-block w-full sm:w-auto py-4 px-6 text-center font-heading font-medium text-base hover:text-green-500 bg-white hover:bg-gray-50 border border-white hover:border-gray-50 rounded-sm transition duration-200" href="#">Send inquiry</a></div>
+    </div>
+  </div>
+</section>
+
+
+            </>
+        </React.Fragment>
+    );
+}
+
---
+++ src/components/newsletter/Test6SectionNewsletter3.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export default function Test6SectionNewsletter3() {
+    return (
+        <React.Fragment>
+            <>
+                <section className="py-16 bg-[url('https://static.shuffle.dev/uploads/files/89/891aed3d3e1847e9f17b379d6d326119edf7f96f/technology-blue.png')] bg-cover bg-center">
+  <div className="container px-4 mx-auto">
+    <div className="flex flex-wrap items-center -mx-4">
+      <div className="w-full lg:w-1/2 px-4 mb-16 lg:mb-0">
+        <div className="max-w-lg mx-auto lg:mx-0 mb-6">
+          <span className="text-sm font-semibold uppercase">
+            <span className="text-white">sign up to our</span>
+            <span className="text-green-500">newsletter</span>
+          </span>
+          <h2 className="font-heading text-3xl sm:text-4xl text-white mt-2">The standard chunk of Lorem Ipsum used since the 1500s</h2>
+        </div>
+      </div>
+      <div className="w-full lg:w-1/2 px-4">
+        <div className="max-w-lg mx-auto lg:mr-0 py-10 px-6 sm:px-12 bg-white rounded-md">
+          <h3 className="font-heading font-medium text-2xl mb-4">Sign up to our newsletter</h3>
+          <p className="leading-7 mb-8">There are many variations of passages of Lorem Ipsum available</p>
+          <form action>
+            <input className="block w-full p-4 mb-4 font-heading text-gray-300 placeholder-gray-300 border border-gray-100 rounded-sm outline-none" type="text" placeholder="Name" />
+            <input className="block w-full p-4 mb-4 font-heading text-gray-300 placeholder-gray-300 border border-gray-100 rounded-sm outline-none" type="email" placeholder="E-mail" />
+            <button className="inline-block w-full py-4 px-6 text-center font-heading font-medium text-white text-base bg-green-500 hover:bg-green-600 rounded-sm transition duration-200" type="submit">Sign up</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+            </>
+        </React.Fragment>
+    );
+}
+
